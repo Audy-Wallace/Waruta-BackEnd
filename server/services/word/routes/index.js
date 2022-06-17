aa68a9b83b509e9c2d6500e03a497d1298226244
WibuSyariah <mspa43@gmail.com>
2022-06-17 15:33:00 +0700

    Deleting unnecessary files

--- server/services/word/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const WordController = require("../controllers/word");
-
-router.get("/", WordController.findAll);
-
-module.exports = router;
