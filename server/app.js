51e817fd990116c663eb5acb279e9cd2a59095f8
redyRenno <redyrenno@gmail.com>
2022-06-19 18:35:45 +0900

    feat: multiplayer turn

--- server/app.js
+++ server/app.js
@@ -7,6 +7,16 @@ const router = require("./routes");
 const port = process.env.PORT || 3000;
 const errorHandler = require("./middlewares/errorHandler");
 const cors = require("cors");
+const path = require("path")
+const http = require("http")
+const {Server} = require("socket.io");
+// perhatikan
+const server = http.createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:8080'
+  }
+})
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -14,6 +24,27 @@ app.use(express.json());
 app.use("/", router);
 app.use(errorHandler);
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  socket.on("connecting", (payload) => {
+    socket.join(payload.roomId);
+    // let temp = {
+    //   username: payload.username,
+    //   socketId: socket.id,
+    //   turn: false
+    // }
+    payload.users = [...payload.users, payload.username]
+    socket.emit("connecting", payload)
+    // console.log(payload, ">>>>>>>>>>>");
+  })
+
+  socket.on("hitAnswer", (payload) => {
+    // console.log(payload);
+    // payload.currentPlayer = payload.username;
+    console.log(payload.username);
+    io.to(payload.roomId).emit("backAnswer", payload)
+  })
+})
+
+server.listen(port, () => {
   console.log("This app is running at port:", port);
 });
