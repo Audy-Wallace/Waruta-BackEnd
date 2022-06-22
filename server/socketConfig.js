5a51284512948d3d03d5fee123dccec476d6ff2a
redyRenno <redyrenno@gmail.com>
2022-06-23 04:36:49 +0900

    implemented web rtc server

--- server/socketConfig.js
+++ server/socketConfig.js
@@ -10,11 +10,16 @@ const io = require("socket.io")(server, {
 });
 let randomIndex = 0;
 io.on("connection", (socket) => {
+  socket.on("joinRoom", (payload) => {
+    socket.join("myRoom");
+    io.to("myRoom").emit("joinRoom", payload.peerId);
+  });
+
   socket.on("joinWaitingRoom", (payload) => {
     socket.join(payload.roomId);
 
     let totalUser = io.sockets.adapter.rooms.get(payload.roomId);
-   
+
     if (totalUser.size === 1) {
       const random = Math.floor(Math.random() * payload.amoutWords); //words.length
       randomIndex = random;
@@ -32,5 +37,9 @@ io.on("connection", (socket) => {
   socket.on("hitAnswer", (payload) => {
     io.to(payload.roomId).emit("backAnswer", payload);
   });
+  //   socket.on('forceDisconnect', function(){
+  //     console.log("disconnect");
+  //     socket.disconnect();
+  // });
 });
 module.exports = io;
